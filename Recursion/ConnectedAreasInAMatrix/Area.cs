613398741898ce3b1b5e49ff068b305d4083ef6c
milen-vm <bay_milen@abv.bg>
2015-09-27 02:50:09 +0300

    Last task solved.

---
+++ Recursion/ConnectedAreasInAMatrix/Area.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Area : IComparable<Area>
+{
+    public int StartRow { get; set; }
+
+    public int StartCol { get; set; }
+
+    public int Size { get; set; }
+
+    public override string ToString()
+    {
+        return string.Format("({0}, {1}), size: {2}", this.StartRow, this.StartCol, this.Size);
+    }
+
+    public int CompareTo(Area other)
+    {
+        int result = this.Size.CompareTo(other.Size) * (-1);
+        if (result == 0)
+        {
+            result = this.StartRow.CompareTo(other.StartRow);
+        }
+
+        if (result == 0)
+        {
+            result = this.StartCol.CompareTo(other.StartCol);
+        }
+
+        return result;
+    }
+}
---
+++ Recursion/ConnectedAreasInAMatrix/ConnectedAreasInAMatrix.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ConnectedAreasInAMatrix
+{
+    private static Tuple<int, int> firstTraversableCell = new Tuple<int, int> (0, 0);
+    private static int areaSize = 0;
+    private static char[,] matrix =
+    //{
+    //    {' ',' ', ' ', '*', ' ', ' ', ' ', '*', ' '},
+    //    {' ',' ', ' ', '*', ' ', ' ', ' ', '*', ' '},
+    //    {' ',' ', ' ', '*', ' ', ' ', ' ', '*', ' '},
+    //    {' ',' ', ' ', ' ', '*', ' ', '*', ' ', ' '}
+    //};
+    {
+        {'*',' ', ' ', '*', ' ', ' ', ' ', '*', ' ', ' '},
+        {'*',' ', ' ', '*', ' ', ' ', ' ', '*', ' ', ' '},
+        {'*',' ', ' ', '*', '*', '*', '*', '*', ' ', ' '},
+        {'*',' ', ' ', '*', ' ', ' ', ' ', '*', ' ', ' '},
+        {'*',' ', ' ', '*', ' ', ' ', ' ', '*', ' ', ' '}
+    };
+
+    static void Main()
+    {
+        var findedAreas = new SortedSet<Area>();
+        FindFirstTraversableCell();
+
+        while (firstTraversableCell != null)
+        {
+            FindConnectedAreas(firstTraversableCell.Item1, firstTraversableCell.Item2);
+            var area = new Area()
+            {
+                StartRow = firstTraversableCell.Item1,
+                StartCol = firstTraversableCell.Item2,
+                Size = areaSize
+            };
+
+            findedAreas.Add(area);
+            areaSize = 0;
+            FindFirstTraversableCell();
+        }
+
+        if (findedAreas.Any())
+        {
+            Console.WriteLine("Total areas found: {0}", findedAreas.Count);
+            int number = 0;
+            foreach (var area in findedAreas)
+            {
+                ++number;
+                Console.WriteLine("Area #{0} at {1}", number, area.ToString());
+            }
+        }
+    }
+
+    private static void FindFirstTraversableCell()
+    {
+        if (firstTraversableCell == null)
+        {
+            return;
+        }
+
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                if (matrix[row, col] == ' ')
+                {
+                    firstTraversableCell = new Tuple<int, int>(row, col);
+                    return;
+                }
+            }
+        }
+
+        firstTraversableCell = null;
+    }
+
+    private static void FindConnectedAreas(int row, int col)
+    {
+        if (!InRange(row, col))
+        {
+            return;
+        }
+
+        if (matrix[row, col] != ' ')
+        {
+            return;
+        }
+
+        matrix[row, col] = 'x';
+        ++areaSize;
+
+        FindConnectedAreas(row, col + 1);   // right direction
+        FindConnectedAreas(row + 1, col);   // down direction
+        FindConnectedAreas(row, col - 1);   // left direction
+        FindConnectedAreas(row - 1, col);   // up direction
+    }
+
+    private static bool InRange(int row, int col)
+    {
+        bool rowInRange = row >= 0 && row < matrix.GetLength(0);
+        bool colInRange = col >= 0 && col < matrix.GetLength(1);
+
+        return rowInRange && colInRange;
+    }
+}
